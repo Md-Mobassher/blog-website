926fe758caccead88034db35a880b32e13c2cd71
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-04-23 16:43:09 +0600

    redux added

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,8 +1,17 @@
 import BlogDetails from "@/components/ui/BlogDetails";
-import { IParams } from "@/type";
+import { IBlog, IParams } from "@/type";
 
+export const generateStaticParams = async () => {
+  const res = await fetch(`http://localhost:5000/blogs`);
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: IBlog) => ({
+    blogId: blog.id,
+  }));
+};
 const BlogDetailsPage = async ({ params }: { params: IParams }) => {
-  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`);
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
   const blog = await res.json();
 
   return (
